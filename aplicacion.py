8151a1386e420e0d28706a79db0931cfa8627de1
dyleitonc <143277627+dyleitonc@users.noreply.github.com>
2025-01-25 11:55:15 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 
 def main():
-  st.title("Clasificació de la base de datos MNIST")
+  st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
 
 if __name__=='__main__':
