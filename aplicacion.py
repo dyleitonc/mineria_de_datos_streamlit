dc46bdf3ffa26dcb22e747ac41332e601d3ff20f
dyleitonc <143277627+dyleitonc@users.noreply.github.com>
2025-01-28 18:22:03 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -4,5 +4,7 @@ def main():
   st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
 
+uploaded_file=st.file_uploaded("Selecciona una imagen (PNG, JPG,JPEG)", type=["jpg","png","jpeg"])
+
 if __name__=='__main__':
   main()
