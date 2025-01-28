82ac74291be1a1f7356f8c472b0ba3ff61365514
dyleitonc <143277627+dyleitonc@users.noreply.github.com>
2025-01-28 18:25:41 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -1,10 +1,15 @@
 import streamlit as st
+from PIL import image 
 
 def main():
   st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
 
   uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
+
+  if uploaded_file is not None:
+    image=Image.open(uploaded_file)
+    st.image(image, caption="Imagen subida")
 
 if __name__ == "__main__":
   main()
