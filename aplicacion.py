8b6143a7f83a8b3c484c727fcb11dd19f0394308
dyleitonc <143277627+dyleitonc@users.noreply.github.com>
2025-01-28 18:22:48 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -1,10 +1,10 @@
-import streamlit as st 
+import streamlit as st
 
 def main():
   st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
 
-uploaded_file=st.file_uploaded("Selecciona una imagen (PNG, JPG,JPEG)", type=["jpg","png","jpeg"])
+  uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
 
-if __name__=='__main__':
+if __name__ == "__main__":
   main()
