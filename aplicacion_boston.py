4b5fb06a17a104b5e35e29624f30f40d5097fcae
dyleitonc <143277627+dyleitonc@users.noreply.github.com>
2025-02-01 08:29:28 -0500

    Update aplicacion_boston.py

--- aplicacion_boston.py
+++ aplicacion_boston.py
@@ -32,7 +32,7 @@ st.markdown(
 image = Image.open('casa.jpg')
 
 # Mostrar la imagen con un tamaño de ancho específico (por ejemplo, 500 píxeles)
-st.image(image, use_container_width=False, width=350)  # Ajusta el valor de width según sea necesario
+st.image(image, use_container_width=False, width=450)  # Ajusta el valor de width según sea necesario
 
 # Dividir las entradas en columnas
 col1, col2 = st.columns(2)
